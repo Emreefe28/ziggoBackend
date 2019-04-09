691dbc438ed274403ad84f46543bb1d97f64456a
JoseNML <33283355+JoseNML@users.noreply.github.com>
2019-04-09 10:58:42 +0200

    users are added to rooms

--- src/main/webapp/chat-server/index.js
+++ src/main/webapp/chat-server/index.js
@@ -8,13 +8,27 @@ let socketIO = require('socket.io');
 let io = socketIO(server);
 
 const port = process.env.PORT || 3000;
+var employees = [];
 
 io.on('connection', (socket) => {
     console.log('user connected');
 
+
     socket.on('new-message', (message) => {
-        io.emit('new-message', message);
+        io.sockets.in(message.room).emit('new-message', message);
+        console.log(message.room)
     });
+
+    socket.on('create', function (room) {
+        socket.join(room);
+        console.log(`user created  room ` + room);
+    });
+
+    socket.on('join', function (room) {
+        socket.join(room);
+        console.log(io.sockets.clients().toString() + ` joined room ` + room);
+    });
+
 });
 
 server.listen(port, () => {

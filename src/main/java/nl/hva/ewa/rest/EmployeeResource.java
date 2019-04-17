0a111eaa38538a21f5915a7f6f8294a48695e2a1
JoseNML <33283355+JoseNML@users.noreply.github.com>
2019-04-17 10:32:17 +0200

    WERKT NIET

--- src/main/java/nl/hva/ewa/rest/EmployeeResource.java
+++ src/main/java/nl/hva/ewa/rest/EmployeeResource.java
@@ -1,20 +1,20 @@
 package nl.hva.ewa.rest;
 
+import nl.hva.ewa.models.User;
 import nl.hva.ewa.services.UserRepository;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
 @Path("/employee") //http://localhost:8080/VodafoneZiggoAPI-1.0/rest/employee
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class EmployeeResource {
     private UserRepository employeeDao;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public Response getEmployees() {
         employeeDao = new UserRepository();
         if (employeeDao.getAllEmployees().isEmpty()) {
@@ -22,5 +22,13 @@ public class EmployeeResource {
         } else {
             return Response.status(Response.Status.OK).entity(employeeDao.getAllEmployees()).build();
         }
+    }
+
+    @POST
+    public void createEmployee(User employee){
+        System.out.println(employee);
+        employeeDao = new UserRepository();
+        employeeDao.saveEmployee(employee);
+
     }
 }
--- src/main/java/nl/hva/ewa/services/UserRepository.java
+++ src/main/java/nl/hva/ewa/services/UserRepository.java
@@ -16,4 +16,11 @@ public class UserRepository {
         Query query = em.createQuery("SELECT e FROM User e WHERE e.role = 2");
         return query.getResultList();
     }
+    public void saveEmployee(User user){
+        User those = new User();
+        those.setName("G");
+        those.setUsername("GORAD");
+        em = new Database().getFactory().createEntityManager();
+        em.persist(those);
+    }
 }

185d40b5d59003cb883314650f5d89015e73a8e1
JoseNML <33283355+JoseNML@users.noreply.github.com>
2019-05-08 10:58:15 +0200

    misc

--- src/main/java/hva/ewa/model/Question.java
+++ src/main/java/hva/ewa/model/Question.java
@@ -5,8 +5,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Question implements Serializable {
